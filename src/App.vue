db04e2ee2771b42afd93f858817e87414ff3872d
Yevhen <shershen.yevhen@gmai>
2023-07-18 16:53:55 +0200

    use RouterView and  RouterLink in component App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,27 @@ import Hooks from './components/hooks/Hooks.vue'
 </script>
 
 <template>
-    <hooks />
+  <div>
+    <header>
+      <!-- Верхняя часть приложения, которая всегда видна -->
+      <div>
+        <RouterLink to="/hooks" class="navigation mr-2">Hooks</RouterLink>
+        <RouterLink to="/router" class="navigation mr-2">Router</RouterLink>
+        <RouterLink to="/" class="navigation">Home</RouterLink>
+      </div>
+    </header>
+    <main>
+      <h1>APP</h1>
+      <!--<RouterView> здесь будет динамически отображать компоненты на основе маршрутов -->
+      <router-view />
+    </main>
+    <footer>
+      <!-- Нижняя часть приложения, которая всегда видна -->
+    </footer>
+  </div>
 </template>
+<style scoped lang="scss">
+.navigation {
+  color: black;
+}
+</style>
---
+++ src/views/RouterView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed, ref } from 'vue'
+</script>
+<template>
+  <div class="w-100 text-center">
+    <h2>ROUTER COMPONENT</h2>
+  </div>
+
+</template>
+
+<!-- View значит что компонент что то отоброжает -->
+
+<!-- Так примерно выглядит упрощенный прототип VUE router
+const url = computed({
+  get({
+    return window.location.href
+  }),
+  set(){
+    window.history.pushState({},'',value)
+  }
+}) -->
